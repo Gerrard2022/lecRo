85ca6f0e5d7e62c56787bd1169812d2697101e57
Gerard <kaberukagerard8@gmail.com>
2024-10-05 22:20:27 +0200

    changed pricing to projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
-// import Pricing from "./sections/Pricing.jsx";
+// import Projects from "./sections/Projects.jsx";
 // import Faq from "./sections/Faq.jsx";
 // import Testimonials from "./sections/Testimonials.jsx";
 // import Download from "./sections/Download.jsx";

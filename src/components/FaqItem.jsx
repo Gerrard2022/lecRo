0c782b762d3da0a6fc5834bf314c82b6deda4ee2
Gerard <kaberukagerard8@gmail.com>
2024-10-08 18:59:08 +0200

    added react collapse

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
+import { Collapse } from "react-collapse"; // Import Collapse instead of SlideDown
 
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
@@ -41,11 +40,10 @@ const FaqItem = ({ item, index }) => {
         </div>
       </div>
 
-      <SlideDown>
-        {activeId === item.id && (
-          <div className="body-3 px-7 py-3.5">{item.answer}</div>
-        )}
-      </SlideDown>
+      {/* Replace SlideDown with Collapse */}
+      <Collapse isOpened={activeId === item.id}>
+        <div className="body-3 px-7 py-3.5">{item.answer}</div>
+      </Collapse>
 
       <div
         className={clsx(
@@ -59,4 +57,5 @@ const FaqItem = ({ item, index }) => {
     </div>
   );
 };
+
 export default FaqItem;

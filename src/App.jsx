bb7494813be013942a8fa644816e20cac77227e6
KABERUKA Gerrard <110633144+Gerrard2022@users.noreply.github.com>
2024-10-10 22:03:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import Header from "./sections/Header.jsx";
-import Hero from "./sections/Hero.jsx";
-import Features from "./sections/Features.jsx";
-import Projects from "./sections/Projects.jsx";
-import Faq from "./sections/Faq.jsx";
-import Team from "./sections/Team.jsx";
-import Footer from "./sections/Footer.jsx";
-
-
 function App() {
-
-  return (
-   <main className='overflow-hidden'>
-      <Header />
-      <Hero />
-      <Features />
-      <Projects />
-      <Team />
-      <Faq />
-      <Footer/>
-   </main>
-  )
+  return <h1 className="text-black">Hello World!</h1>;
 }
 
-export default App
+export default App;

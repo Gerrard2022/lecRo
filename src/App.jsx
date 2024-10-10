7946a782e2cb4a921702cdca19ec12439ff4b603
KABERUKA Gerrard <110633144+Gerrard2022@users.noreply.github.com>
2024-10-10 22:06:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Projects from "./sections/Projects.jsx";
+import Faq from "./sections/Faq.jsx";
+import Team from "./sections/Team.jsx";
+import Footer from "./sections/Footer.jsx";
+
+
 function App() {
-  return <h1 className="text-black">Hello World!</h1>;
+
+  return (
+   <main>
+      <Header />
+      <Hero />
+      <Features />
+      <Projects />
+      <Team />
+      <Faq />
+      <Footer/>
+   </main>
+  )
 }
 
-export default App;
+export default App
+
